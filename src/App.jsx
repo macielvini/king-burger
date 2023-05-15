f521d7651b1cd7c8821e6fbefd7a32f8537f7bea
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-15 16:27:18 -0300

    chore: toaster setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./styles/global.css";
-import Login from "./pages/Login";
 import Routes from "./routes";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const [signed, _] = useState(false);
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="bg-gray-950 min-h-screen text-white">
       <Routes />
+      <Toaster />
     </div>
   );
 }
