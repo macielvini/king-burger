36950ebd6794c4ccda72e53071a784e315d9196c
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-15 16:55:36 -0300

    feat: success page auto redirect

--- src/pages/Register/success.jsx
+++ src/pages/Register/success.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import * as Icons from "@tabler/icons-react";
 
 function Success() {
-  const [count, setCount] = useState(5);
+  const [count, setCount] = useState(3);
+  const navigate = useNavigate();
 
   function countdown() {
     setTimeout(() => {
       if (count > 0) {
         setCount(count - 1);
       }
+      if (count <= 0) return navigate("/");
     }, 1000);
   }
 
@@ -15,12 +19,20 @@ function Success() {
 
   return (
     <div className="text-center flex h-screen items-center justify-center flex-col md:p-16 px-12 py-16 gap-8">
-      <h1 className="font-bold text-3xl ">Conta criado com sucesso!</h1>
+      <h1 className="font-bold text-3xl flex items-center gap-2">
+        <Icons.IconCircleCheckFilled
+          size={60}
+          className="text-green-500 animate-bounce"
+        />
+        Conta criado com sucesso!
+      </h1>
       <p>Em alguns segundos vamos te redirecionar para página de login!</p>
       <p className="font-bold text-6xl bg-accent px-4 py-2 rounded-lg">
         {count}
       </p>
-      <button className="btn-primary mt-3">Ir agora!</button>
+      <Link to={"/"}>
+        <button className="btn-primary mt-3">Ir agora!</button>
+      </Link>
     </div>
   );
 }
