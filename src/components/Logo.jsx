819d667f43216bdb96bc81f5d66b85372e9e0b87
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-16 20:31:26 -0300

    fix: logo name

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,7 +6,7 @@ function Logo({ size = "sm" }) {
   return (
     <div
       className={clsx(
-        "font-secondary mx-8 min-w-max font-bold flex justify-center items-center",
+        "font-secondary text-accent mx-8 min-w-max font-bold flex justify-center items-center",
         { "text-xl gap-2": size === "sm" },
         { "text-4xl md:text-5xl gap-4": size === "lg" }
       )}
@@ -19,7 +19,7 @@ function Logo({ size = "sm" }) {
           { "md:h-16 h-10": size === "lg" }
         )}
       />
-      food explorer
+      KING BURGER
     </div>
   );
 }
