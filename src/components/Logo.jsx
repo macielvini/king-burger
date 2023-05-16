bbb7a30f83695ccaafe907cda8942f9a7a6061f1
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-16 18:13:27 -0300

    feat: logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "../assets/logo/logo.svg";
+import clsx from "clsx";
+
+function Logo({ size = "sm" }) {
+  return (
+    <div
+      className={clsx(
+        "font-secondary mx-8 min-w-fit font-bold flex justify-center items-center",
+        { "text-xl gap-2": size === "sm" },
+        { "text-4xl md:text-5xl gap-4": size === "lg" }
+      )}
+    >
+      <img
+        src={logo}
+        alt="logo"
+        className={clsx(
+          { "h-6": size === "sm" },
+          { "md:h-16 h-10": size === "lg" }
+        )}
+      />
+      food explorer
+    </div>
+  );
+}
+
+export default Logo;
