da7bdb14f31b28661d20b4050820c3affe09eb5d
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-16 23:11:44 -0300

    feat: home banner

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,9 @@ import AuthProvider from "./contexts/authContext";
 function App() {
   const [signed, _] = useState(false);
 
-  // w-screen h-24 flex justify-center items-center bg-secondary fixed top-0 left-0 md:px-20 xl:px-28 lg:h-28
-
   return (
     <AuthProvider>
-      <div className="bg-background min-h-screen max-w-screen-lg m-auto text-ink px-6 md:px-20 xl:px-28">
+      <div className="bg-background min-h-screen max-w-screen-lg m-auto text-ink mt-28 lg:mt-32 px-6 md:px-20 xl:px-28 flex flex-col items-center">
         <Routes />
         <Toaster />
       </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import Header from "../../components/Header";
+import banner from "../../assets/banners/banner1.png";
 
 function Home() {
   const [cartCount, setCartCount] = useState(0);
 
   return (
-    <>
+    <div className="">
       <Header cartCount={cartCount} />
-      <div className="pt-32">item</div>
-    </>
+      <div className="aspect-video max-w-screen-lg rounded-lg overflow-hidden">
+        <img
+          src="./src/assets/banners/banner1.png"
+          alt="banner de comida"
+          className="object-contain w-full h-full"
+        />
+      </div>
+    </div>
   );
 }
 
