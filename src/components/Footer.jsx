b29c62e60c258c411622c8c45d2070da7d8c13fc
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-21 19:12:05 -0300

    fix: footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ function Footer() {
   return (
     <footer className="w-screen h-20 bg-secondary flex items-center justify-center self-end">
       <div className="max-w-screen-lg md:px-20 lg:px-16 xl:px-0 px-6 w-full flex justify-between">
-        <Logo size="sm" />
+        <Logo size="xs" />
         <p>
-          2023 - Projeto criado por{" "}
+          2023 - <span className="sm:inline hidden">Projeto criado por </span>
           <a
             className="underline underline-offset-4 font-bold"
             href="https://github.com/macielvini/"
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,15 +6,17 @@ function Logo({ size = "sm" }) {
   return (
     <div
       className={clsx(
-        "font-secondary text-accent min-w-max font-bold flex items-center",
-        { "text-xl gap-2": size === "sm" },
-        { "text-4xl md:text-5xl gap-4": size === "lg" }
+        "font-secondary text-accent sm:min-w-max font-bold flex items-center",
+        { "text-xl gap-3": size === "sm" },
+        { "text-4xl md:text-5xl gap-4": size === "lg" },
+        { "text-sm leading-tight gap-2": size === "xs" }
       )}
     >
       <img
         src={logo}
         alt="logo"
         className={clsx(
+          "h-6",
           { "h-6": size === "sm" },
           { "md:h-16 h-10": size === "lg" }
         )}
