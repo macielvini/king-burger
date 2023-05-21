9d284f45243a0b9e5e8d53fb4878e95c2dcbd648
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-21 20:35:48 -0300

    feat: items desktop layout adjust

--- src/pages/Item/index.jsx
+++ src/pages/Item/index.jsx
@@ -38,7 +38,7 @@ function Item() {
   return (
     <>
       <Header />
-      <div className="w-full flex flex-col pt-28 lg:pt-32 pb-8">
+      <div className="w-full flex flex-col pt-28 lg:pt-32 pb-8 lg:pb-0">
         <div
           className=" w-fit py-1 pr-2 select-none rounded-full flex items-center gap-2 text-xl mb-3 active:bg-secondary  transition-all"
           onClick={() => navigate(-1)}
@@ -46,25 +46,22 @@ function Item() {
           <Icons.IconChevronLeft />
           voltar
         </div>
-        <div className="flex flex-col items-center gap-6 ">
-          <div className="w-[264px] h-[264px]">
+        <div className="grid justify-center justify-items-center lg:justify-start gap-6 lg:grid-cols-2 content-center lg:items-center">
+          <div className="w-full h-[264px] overflow-hidden lg:h-[390px] lg:w-full lg:row-span-4">
             {item && (
               <img
                 src={item.image}
                 alt=""
-                className={clsx(
-                  "object-cover object-center h-full w-full scale-110",
-                  {
-                    block: item.image,
-                  }
-                )}
+                className={clsx("object-cover h-full w-full", {
+                  block: item.image,
+                })}
               />
             )}
           </div>
-          <p className="font-bold text-3xl w-full text-center text-accent">
+          <p className="font-bold text-3xl w-full text-center text-accent lg:text-left lg:text-5xl font-secondary">
             {item && item.name}
           </p>
-          <div className="flex max-w-[300px] gap-6 flex-wrap">
+          <div className="flex max-w-[300px] gap-3 flex-wrap lg:max-w-full">
             {item &&
               item.ingredients.map((ingredient) => (
                 <p
@@ -75,13 +72,13 @@ function Item() {
                 </p>
               ))}
           </div>
-          <div className="flex gap-4">
+          <div className="flex gap-4 lg:justify-self-start">
             <div className="flex items-center text-2xl gap-4 bg-secondary bg-opacity-70 py-1 px-2 rounded-md select-none">
               <Icons.IconMinus onClick={minus} />
               <span>{count < 10 ? "0" + count.toString() : count}</span>
               <Icons.IconPlus onClick={plus} />
             </div>
-            <button className="btn-primary flex items-center gap-2">
+            <button className="btn-primary flex items-center gap-2 max-w-max ">
               <Icons.IconReceipt />
               pedir
               <Icons.IconPointFilled size={10} />
