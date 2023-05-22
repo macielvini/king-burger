9f69d5e207feff209f9540476878c772419e7358
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-22 17:13:55 -0300

    feat: skeleton loading

--- src/pages/Item/index.jsx
+++ src/pages/Item/index.jsx
@@ -5,19 +5,28 @@ import { itemsApi } from "../../services/api/itemsApi";
 import * as Icons from "@tabler/icons-react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import { sleep } from "../../utils/sleep";
+import { toast } from "react-hot-toast";
 
 function Item() {
   const { id } = useParams();
   const [item, setItem] = useState(undefined);
   const [count, setCount] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
 
   async function fetchItem() {
+    setIsLoading(true);
     try {
+      //SIMULATE API DELAY
+      await sleep(1000);
       const res = await itemsApi.findItemById(id);
       setItem(res);
+      setIsLoading(false);
     } catch (error) {
-      console.log(error.response);
+      toast.error("Erro ao carregar produto. Voltando a tela inicial...");
+      await sleep(2000);
+      navigate(-1);
     }
   }
 
@@ -48,7 +57,9 @@ function Item() {
         </div>
         <div className="grid justify-center justify-items-center lg:justify-start gap-6 lg:grid-cols-2 content-center lg:items-center">
           <div className="w-full h-[264px] overflow-hidden lg:h-[390px] lg:w-full lg:row-span-4">
-            {item && (
+            {isLoading ? (
+              <div className="aspect-square m-auto h-full rounded-full bg-gray-200 animate-pulse" />
+            ) : (
               <img
                 src={item.image}
                 alt=""
@@ -58,35 +69,58 @@ function Item() {
               />
             )}
           </div>
-          <p className="font-bold text-3xl w-full text-center text-accent lg:text-left lg:text-5xl font-secondary">
-            {item && item.name}
-          </p>
-          <div className="flex max-w-[300px] gap-3 flex-wrap lg:max-w-full">
-            {item &&
-              item.ingredients.map((ingredient) => (
-                <p
-                  key={ingredient.id}
-                  className="py-1 px-2 bg-secondary bg-opacity-70 min-w-max rounded-sm"
-                >
-                  {ingredient.name}
-                </p>
-              ))}
+          {isLoading ? (
+            <div className="animate-pulse lg:justify-self-start bg-gray-200 w-[220px] h-12 rounded-md" />
+          ) : (
+            <p className="font-bold text-3xl w-full text-center text-accent lg:text-left lg:text-5xl font-secondary">
+              {item && item.name}
+            </p>
+          )}
+          <div className="flex max-w-[300px] gap-3 flex-wrap lg:min-w-full lg:place-items-start">
+            {isLoading
+              ? Array.from({ length: 3 }).map((_, i) => (
+                  <div
+                    key={i}
+                    className="animate-pulse bg-gray-200 w-[80px] h-8 rounded-sm"
+                  />
+                ))
+              : item.ingredients.map((ingredient) => (
+                  <p
+                    key={ingredient.id}
+                    className="py-1 px-2 bg-secondary bg-opacity-70 min-w-max rounded-sm"
+                  >
+                    {ingredient.name}
+                  </p>
+                ))}
           </div>
           <div className="flex gap-4 lg:justify-self-start">
-            <div className="flex items-center text-2xl gap-4 bg-secondary bg-opacity-70 py-1 px-2 rounded-md select-none">
-              <Icons.IconMinus onClick={minus} />
-              <span>{count < 10 ? "0" + count.toString() : count}</span>
-              <Icons.IconPlus onClick={plus} />
-            </div>
-            <button className="btn-primary flex items-center gap-2 max-w-max ">
-              <Icons.IconReceipt />
-              pedir
-              <Icons.IconPointFilled size={10} />
-              {new Intl.NumberFormat("pt-br", {
-                style: "currency",
-                currency: "BRL",
-              }).format(item && item.price)}
-            </button>
+            {isLoading ? (
+              <>
+                <div className="animate-pulse w-[124px] h-12 bg-gray-200 rounded-md" />
+              </>
+            ) : (
+              <div className="flex items-center text-2xl gap-4 bg-secondary bg-opacity-70 py-1 px-2 rounded-md select-none">
+                <Icons.IconMinus onClick={minus} />
+                <span>{count < 10 ? "0" + count.toString() : count}</span>
+                <Icons.IconPlus onClick={plus} />
+              </div>
+            )}
+
+            {isLoading ? (
+              <>
+                <div className="animate-pulse w-[180px] h-12 bg-gray-200 rounded-md" />
+              </>
+            ) : (
+              <button className="btn-primary flex items-center gap-2 max-w-max ">
+                <Icons.IconReceipt />
+                pedir
+                <Icons.IconPointFilled size={10} />
+                {new Intl.NumberFormat("pt-br", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(item && item.price)}
+              </button>
+            )}
           </div>
         </div>
       </div>
