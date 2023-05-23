4aeefd49e2da817714eafb3779e300912d89378f
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-22 21:45:30 -0300

    fix: item app route

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 
 import React from "react";
 import Home from "../pages/Home";
+import Item from "../pages/Item";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/item/:id" element={<Item />} />
     </Routes>
   );
 }
